feb87841253703bd11f2182687bf7e8c234ee153
Pilz <48645439+pilz0@users.noreply.github.com>
2024-06-01 17:13:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 cargo build --release
-./target/release/pixelflut-gaming resources/foerri.png 850 150
+./target/release/pixelflut-gaming resources/mon.png 1600 130
 # target/release/pixelflut-gaming resources/cs.jpg 300 200
